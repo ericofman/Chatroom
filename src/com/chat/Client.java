7ab983746ccf59ae0178de552a0263598bbf2012
ericofman <eric.ofman@hotmail.com>
2016-10-08 23:29:54 -0400

    Project commit

---
+++ src/com/chat/Client.java
@@ -0,0 +1,105 @@
+package com.chat;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
+import javax.swing.BoxLayout;
+import java.awt.FlowLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class Client extends JFrame { 
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane; 
+ 
+	private String name, password, ipaddress;
+	private int port;
+	
+	private JButton btnSend;
+	private JTextArea textArea;
+	private JTextField txtMessage;
+	
+	public Client(String name, String password, String ipaddress, int port) {
+		this.name = name;
+		this.password = password;
+		this.ipaddress = ipaddress;
+		this.port = port;
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(700, 500);
+		setLocationRelativeTo(null);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{245, 0, 1, 130, 75, 0};
+		gbl_contentPane.rowHeights = new int[]{29, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_contentPane.columnWeights = new double[]{1.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
+		
+		textArea = new JTextArea();
+		textArea.setEditable(false);
+		GridBagConstraints gbc_textArea = new GridBagConstraints();
+		gbc_textArea.gridheight = 15;
+		gbc_textArea.gridwidth = 5;
+		gbc_textArea.insets = new Insets(0, 0, 5, 0);
+		gbc_textArea.fill = GridBagConstraints.BOTH;
+		gbc_textArea.gridx = 0;
+		gbc_textArea.gridy = 0;
+		contentPane.add(textArea, gbc_textArea);
+		
+		txtMessage = new JTextField();
+		txtMessage.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if(e.getKeyCode() == KeyEvent.VK_ENTER) {
+					pushMessage(name, txtMessage.getText());
+				}
+			}
+		});
+		GridBagConstraints gbc_txtMessage = new GridBagConstraints();
+		gbc_txtMessage.gridwidth = 4;
+		gbc_txtMessage.insets = new Insets(0, 0, 0, 5);
+		gbc_txtMessage.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtMessage.gridx = 0;
+		gbc_txtMessage.gridy = 15;
+		contentPane.add(txtMessage, gbc_txtMessage);
+		txtMessage.setColumns(10);
+		
+		btnSend = new JButton("Send");
+	
+		btnSend.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) { 
+				pushMessage(name, txtMessage.getText());
+			}
+		});
+		GridBagConstraints gbc_btnSend = new GridBagConstraints();
+		gbc_btnSend.gridx = 4;
+		gbc_btnSend.gridy = 15;
+		contentPane.add(btnSend, gbc_btnSend);
+		setVisible(true);
+		setTitle("Chat Client");
+		
+		txtMessage.requestFocusInWindow();
+	}
+	
+	public void pushMessage(String user, String msg) {
+		if(msg.isEmpty()) return;
+		String seperator = System.getProperty("line.separator"); 
+		textArea.append(msg + seperator);
+		txtMessage.setText("");
+	}
+}
---
+++ src/com/chat/Login.java
@@ -0,0 +1,134 @@
+package com.chat;
+
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.border.EmptyBorder;
+
+public class Login extends JFrame { 
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField txtUsername;
+	private JPasswordField txtPassword;
+	private JLabel lblIpAddress;
+	private JTextField txtAddress;
+	private JTextField txtPort;
+	private JLabel lblPort;
+	private JButton btnHost;
+	private JLabel lblLogin_1;
+ 
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login frame = new Login();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	} 
+	
+	public void login(String name, String password, String address, int port) { 
+		dispose();
+		new Client(name, password, address, port);
+	}
+	
+	public Login() {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException e) { 
+			e.printStackTrace();
+		} catch (InstantiationException e) { 
+			e.printStackTrace();
+		} catch (IllegalAccessException e) { 
+			e.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e) { 
+			e.printStackTrace();
+		}
+		setResizable(false);
+		setTitle("Login");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		setSize(366, 343);
+		setLocationRelativeTo(null);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		txtUsername = new JTextField();
+		txtUsername.setText("Username");
+		txtUsername.setBounds(160, 49, 130, 26);
+		contentPane.add(txtUsername);
+		txtUsername.setColumns(10);
+		
+		txtPassword = new JPasswordField();
+		txtPassword.setText("Password"); 
+		txtPassword.setBounds(160, 77, 130, 26);
+		contentPane.add(txtPassword);
+		txtPassword.setColumns(10);
+		
+		JLabel lblLogin = new JLabel("Username:");
+		lblLogin.setBounds(77, 54, 71, 16);
+		contentPane.add(lblLogin);
+		
+		JLabel lblPassword = new JLabel("Password:");
+		lblPassword.setBounds(77, 82, 71, 16);
+		contentPane.add(lblPassword);
+		
+		JButton btnConnect = new JButton("Connect");
+		btnConnect.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String username = txtUsername.getText();
+				String password = String.valueOf(txtPassword.getPassword());
+				String ipaddress = txtAddress.getText();
+				int port = -1;
+				try {
+					port = Integer.parseInt(txtPort.getText());
+				} catch(NumberFormatException nfe) {
+					port = -1;
+				}
+				login(username, password, ipaddress, port);
+			}
+		});
+		btnConnect.setBounds(77, 268, 96, 29);
+		contentPane.add(btnConnect);
+		
+		lblIpAddress = new JLabel("IP Address:");
+		lblIpAddress.setBounds(147, 135, 71, 16);
+		contentPane.add(lblIpAddress);
+		
+		txtAddress = new JTextField();
+		txtAddress.setBounds(121, 154, 130, 26);
+		contentPane.add(txtAddress);
+		txtAddress.setColumns(10);
+		
+		txtPort = new JTextField();
+		txtPort.setBounds(121, 211, 130, 26);
+		contentPane.add(txtPort);
+		txtPort.setColumns(10);
+		
+		lblPort = new JLabel("Port:");
+		lblPort.setBounds(168, 192, 29, 16);
+		contentPane.add(lblPort);
+		
+		btnHost = new JButton("Host");
+		btnHost.setBounds(201, 268, 89, 29);
+		contentPane.add(btnHost);
+		
+		lblLogin_1 = new JLabel("LOGIN");
+		lblLogin_1.setBounds(163, 21, 40, 16);
+		contentPane.add(lblLogin_1);
+	}
+}
